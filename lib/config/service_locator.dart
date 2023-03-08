010de137e76557aa795848fdea789586da752122
radyhaggag <radyhaggag948@gmail.com>
2023-03-08 03:30:30 +0200

    edit file name

--- lib/config/service_locator.dart
+++ lib/config/service_locator.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:ghaslah/features/auth/data/services/auth_services.dart';
-import 'package:ghaslah/features/home/data/services/auth_services.dart';
+import 'package:ghaslah/features/home/data/services/home_services.dart';
 
 import '../core/api/api_service.dart';
 import '../core/api/dio_api_service.dart';
